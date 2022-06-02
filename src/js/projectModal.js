bbbe14c74bb7f3dd4187fe93cd38e24885988591
SzarneckiP <siarniuk@gmail.com>
2022-06-02 05:00:58 +0200

    fix open and close modal

--- src/js/projectModal.js
+++ src/js/projectModal.js
@@ -1,28 +1,16 @@
-document.addEventListener('DOMContentLoaded', () => {
+const openModal = (id) => {
+    const modalId = document.querySelector(`#${id}`)
+    modalId.classList.add('active-modal')
+}
 
-    var modal = document.getElementById("modalProjectBed");
-
-    // Get the button that opens the modal
-    var btn = document.getElementById("btnProjectBed");
+const closeModal = () => {
+    const openedModal = document.querySelector('.active-modal')
+    openedModal.classList.remove('active-modal')
+}
     
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
-    
-    // When the user clicks on the button, open the modal
-    btn.onclick = function() {
-      modal.style.display = "block";
+window.onclick = function(event) {
+    const openedModal = document.querySelector('.active-modal')
+    if (event.target == openedModal) {
+        openedModal.classList.remove('active-modal')
     }
-    
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-      modal.style.display = "none";
-    }
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
-    
-})
+}
